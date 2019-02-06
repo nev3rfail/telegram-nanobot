7e6079ad3bab41bfc7254ea123ab48ee7d7e980a
Artem M <nev3rfail@gmail.com>
2019-02-06 20:15:36 +0000

    added simple cache class

---
+++ util.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# encoding=utf8
+
+from collections import namedtuple
+import time
+
+class Stupidcache:
+    def __init__(self, update_function=None, ttl=36000):
+        self.update_function = update_function
+        self.ttl = ttl
+        self.in_progress = False
+        storeunit = namedtuple("item", "data created")
+        self.pool = storeunit(data={}, created=0)
+
+
+    def get_data(self):
+        now = int(time.time())
+        refill = False
+        if now > self.pool.created + self.ttl:
+            print "TTL reached, refill required."
+            refill = True
+
+        if refill and not self.in_progress:
+            self.in_progress = True
+            newdata = self.update_function()
+            if newdata:
+                self.pool = self.pool._replace(data=newdata, created=int(time.time()))
+            self.in_progress = False
+
+        return self.pool.data
