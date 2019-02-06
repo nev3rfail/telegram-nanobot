9ef6b9ad561d36ede48e38d6edf2cff144034685
Artem M <nev3rfail@gmail.com>
2019-02-06 17:35:39 +0000

    Bot skeleton with plugin loader

---
+++ __init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import os
+import importlib
+import json
+
+bot_path = os.path.dirname(os.path.abspath(__file__))
+"""We use patched pytelegrambotapi here with sendAnimation and video previews"""
+if os.path.is_dir(bot_path + '/pyTelegramBotApi'):
+    sys.path.insert(0, bot_path + '/pyTelegramBotAPI')
+import telebot as telebot
+
+
+with open(bot_path + '/config.json') as json_file:
+    settings = json.loads(json_file.read())
+
+
+if __name__ != "__main__":
+    raise Exception("This file should be executed, not imported.")
+
+bot = telebot.TeleBot(settings['telegram_token'])
+
+sys.path.append(bot_path + '/plugins')
+loaded = {}
+if len(settings['plugins']):
+    for plugin in settings['plugins']:
+        loaded[plugin] = importlib.import_module("text_vertical")
+        loaded[plugin].register(bot)
+
+
+bot.polling()
