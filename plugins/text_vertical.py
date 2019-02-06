fd1c6323838f384ec8dfe1f6300d7251c773a44e
Artem M <nev3rfail@gmail.com>
2019-02-06 18:43:15 +0000

    vertical text plugin

---
+++ plugins/text_vertical.py
@@ -0,0 +1,31 @@
+#encoding: UTF-8
+from telebot import types
+
+def register(bot):
+    print "Loading vertical text plugin"
+    @bot.channel_post_handler(func=lambda m: m.text.startswith("!vertical ") or m.text.startswith('!vert '))
+    @bot.message_handler(func=lambda m: m.text.startswith("!vertical ") or m.text.startswith('!vert '))
+    @bot.message_handler(commands=['vertical', 'vert'])
+    @bot.channel_post_handler(commands=['vertical', 'vert'])
+    def vertical(msg):
+        msgs = msg.text.split(" ")
+        msgs.pop(0)
+        message = " ".join(msgs)
+        if len(message):
+            bot.send_message(msg.chat.id, "```\n" + do_vertical(message) + "```", parse_mode="Markdown")
+
+
+def do_vertical(msg):
+    s = msg
+    sb = ""
+    for i in range(len(s)):
+        sb += s[i] + " "
+    spaces = " "
+    sb += '\n'
+    for i in range(1, len(s)):
+        sb += s[i] + spaces + s[i] + '\n'
+        spaces += "  "
+    return sb
+
+def handle_inline(msg):
+    return types.InlineQueryResultArticle('1', '!vert', types.InputTextMessageContent("```\n" + do_vertical(msg) + "```", parse_mode="Markdown"))
