45c2d4ddd3504e055da0a81d843c727e50ef1a80
Artem M <nev3rfail@gmail.com>
2019-02-16 10:23:47 +0000

    dummy hello pluginn rewritten

--- plugins/hello.py
+++ plugins/hello.py
@@ -1,10 +1,21 @@
 #encoding: UTF-8
 
-def register( ** kwargs):
-    bot = kwargs['bot']
-    @bot.message_handler(commands=['hello'])
-    @bot.channel_post_handler(commands=['hello'])
-    def send_welcome(message):
-	bot.reply_to(message, "Hello, world!")
+__plugin_name__ = "Dummy hello"
+
+def register(bot, listen=True, config={}, ** kwargs):
+    if listen:
+        @bot.channel_post_handler(regexp="^!hello$")
+        @bot.message_handler(regexp="^!hello$")
+        @bot.message_handler(commands=['hello'])
+        @bot.channel_post_handler(commands=['hello'])
+        def send_welcome(message):
+            if "text" in config:
+                bot.reply_to(message, config["text"])
+            else:
+                bot.reply_to(message, "Hello, world!")
+
+
+def helpmsg():
+    return "`/hello` for hello message"
 
 
