7595b8f700d7fd2eab90df93299bb858c1cabb52
Artem M <nev3rfail@gmail.com>
2019-02-17 14:36:53 +0000

    we now can ask for id of last insert

--- libs/db.py
+++ libs/db.py
@@ -5,13 +5,20 @@ class Database:
         self.dbconnection.row_factory = sqlite3.Row
 
     def query(self, * args, ** kwargs):
+        get_id = None
+        if "get_id" in kwargs:
+            get_id = True
+            del kwargs['get_id']
+
         #https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.execute
         if len(args) == 2:
             cursor = self.dbconnection.execute(args[0], args[1])
         else:
             cursor = self.dbconnection.execute(args[0])
 
-        if cursor.rowcount != -1:
+        if get_id and cursor.lastrowid:
+            return cursor.lastrowid
+        elif cursor.rowcount != -1:
             return cursor.rowcount
         else:
             return cursor.fetchall()
