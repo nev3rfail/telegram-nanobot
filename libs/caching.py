d98ff0f6899948896c8d70eba53bca1f3f0667ec
Artem M <nev3rfail@gmail.com>
2019-05-14 12:34:09 +0000

    multipool added

--- libs/caching.py
+++ libs/caching.py
@@ -2,6 +2,7 @@
 # encoding=utf8
 
 from collections import namedtuple
+import re
 import time
 
 class Pool:
@@ -28,3 +29,62 @@ class Pool:
             self.in_progress = False
 
         return self.pool.data
+
+class MultiPool:
+
+    def __init__(self, update_function=None, ttl=36000):
+        self.update_function = update_function
+        self.ttl = ttl
+        self.pool = {}
+        self.in_progress = []
+        self.storeunit = namedtuple("item", "data created")
+        '''
+        pool format is url => ['data' => [], 'created' => int]
+        '''
+
+    def get_key(self, url):
+        return re.sub("&after=(.*)$", "", re.sub("\?after=(.*)$", "", url))
+
+
+    def get_data(self, url, moar=False):
+        now = int(time.time())
+        key = self.get_key(url)
+        print("\nKey is", key)
+        if key not in self.pool:
+            print("Key not in pool, fillinng it.")
+            self.refill(url)
+            return self.pool[key].data
+
+        refill = False
+
+        if self.pool[key].created > 0 and now > self.pool[key].created + self.ttl:
+            print("TTL revalidation, cache+" + str(self.ttl) + " is older than now for a", now-self.pool[key].created + self.ttl, "s.")
+            refill = True
+        if refill:
+            self.refill(url)
+
+        if not refill and moar:
+            self.append(url)
+
+        return self.pool[key].data
+
+    def refill(self, url):
+        if url not in self.in_progress:
+            self.in_progress.append(url)
+            print("Refilling cache from " + url + "...")
+            key = self.get_key(url)
+            if key not in self.pool:
+                self.pool[key] = self.storeunit(data=[], created=0)
+            self.pool[key] = self.pool[key]._replace(data=self.update_function(url), created=int(time.time()))
+            self.in_progress.remove(url)
+        else:
+            print(url, "is already refilling.")
+
+    def append(self, url):
+        key = self.get_key(url)
+        if key not in self.pool:
+            return self.refill(url)
+        #print self.pool[key]
+        self.pool[key]._replace(data=self.pool[key].data.extend(self.update_function(url)), created=int(time.time()))
+        #self.pool[key]['data'].extend(self.do_request(url))
+        #self.pool[key]['data']['created'] = int(time.time())
