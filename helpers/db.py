b56e2c92bd914d3621c5c50e2883e37efa651415
Artem M <nev3rfail@gmail.com>
2019-05-14 13:19:28 +0000

    autocommit fixed again

--- helpers/db.py
+++ helpers/db.py
@@ -2,11 +2,11 @@ from libs.db import Database
 
 _instance = None
 
-def instance(dbpath=None):
+def instance(dbpath=None, autocommit=False):
     global _instance
     if not _instance:
         if dbpath:
-            _instance = Database(dbpath)
+            _instance = Database(dbpath, autocommit)
             print("Database connected")
         else:
             raise ValueError("No database connection.")
